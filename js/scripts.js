2ced197f9835a7c16b75d1f67835e6b63a6aaa9c
jepurdy313 <69768195+jepurdy313@users.noreply.github.com>
2020-09-21 22:04:04 -0400

    Introduced first project JS code
    The following changes were made to /scripts.js:
    
    -Removed all existing code
    -Added array variable 'pokemonList'
    -Added 9 objects inside array 'pokemonList'
    -Added the following 5 key-values to each object (name, pokedexNumber, height, abilities, and type)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,56 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood)
+let pokemonList = [
+    {name: 'Bulbasaur',
+    pokedexNumber: 1,
+    height: 0.7,
+    abilities: ['Chlorophyll', 'Overgrow'],
+    type: ['grass', 'poison']
+    },
+    {name: 'Ivysaur',
+    pokedexNumber: 2,
+    height: 1,
+    abilities: ['Chlorophyll', 'Overgrow'],
+    type: ['grass', 'poison']
+    },
+    {name: 'Venusaur',
+    pokedexNumber: 3,
+    height: 2,
+    abilities: ['Clorophyll', 'Overgrow'],
+    type: ['grass', 'poison'],
+    },
+    {name: 'Charmander',
+    pokedexNumber: 4,
+    height: 0.6,
+    abilities: ['Blaze','Solar-power'],
+    type: ['fire']
+    },
+    {name: 'Charmeleon',
+    pokedexNumber: 5,
+    height: 1.1,
+    abilities: ['Blaze', 'Solar-power'],
+    type: ['fire']
+    },
+    {name: 'Charizard',
+    pokedexNumber: 6,
+    height: 1.7,
+    abilities: ['Blaze', 'Solar-power'],
+    type: ['fire', 'flying']
+    },
+    {name: 'Squirtle',
+    pokedexNumber: 7,
+    height: 0.5,
+    abilities: ['Rain-dish', 'Torrent'],
+    type: ['water']
+    },
+    {name: 'Wartortle',
+    pokedexNumber: 8,
+    height: 1,
+    abilities: ['Rain-dish', 'Torrent'],
+    type: ['water']
+    },
+    {name: 'Blastoise',
+    pokedexNumber: 9,
+    height: 1.6,
+    abilities: ['Rain-dish', 'Torrent'],
+    type: ['water']
+    }
+]
