1463ab400030e1239ac25c486776e68d1e3ce60f
jepurdy313 <69768195+jepurdy313@users.noreply.github.com>
2021-04-30 15:26:24 -0400

    Added API and AJAX Support
    General:
    -Added files: js\fetch-polyfill.js & js\promise-polyfill.js
    
    \index.html
    -Added script tags to call polyfills for older browser support
    
    \js\scripts.js
    -Removed static array of pokemon details from pokemonList
    -Fetched pokemon API
    -Added function loadList to return JSON results from fetched information from API
    -Added function loadDetails to load additional information for each result returned by loadList
    -Updated function showDetails to fetch information from loadDetails instead of the now empty pokemonList array
    -Added to the list of functions supported by the return function
    -Added code that wrote to DOM using the newly added loadList function
    -Removed dead code

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,8 @@
 //wrapped pokemonList inside IIFE
 var pokemonRepository = (function () {
     //list of pokemon
-    let pokemonList = [
-        {name: 'Bulbasaur',
-        pokedexNumber: 1,
-        height: 0.7,
-        abilities: ['Chlorophyll', 'Overgrow'],
-        type: ['grass', 'poison']
-        },
-        {name: 'Ivysaur',
-        pokedexNumber: 2,
-        height: 1,
-        abilities: ['Chlorophyll', 'Overgrow'],
-        type: ['grass', 'poison']
-        },
-        {name: 'Venusaur',
-        pokedexNumber: 3,
-        height: 2,
-        abilities: ['Clorophyll', 'Overgrow'],
-        type: ['grass', 'poison'],
-        },
-        {name: 'Charmander',
-        pokedexNumber: 4,
-        height: 0.6,
-        abilities: ['Blaze','Solar-power'],
-        type: ['fire']
-        },
-        {name: 'Charmeleon',
-        pokedexNumber: 5,
-        height: 1.1,
-        abilities: ['Blaze', 'Solar-power'],
-        type: ['fire']
-        },
-        {name: 'Charizard',
-        pokedexNumber: 6,
-        height: 1.7,
-        abilities: ['Blaze', 'Solar-power'],
-        type: ['fire', 'flying']
-        },
-        {name: 'Squirtle',
-        pokedexNumber: 7,
-        height: 0.5,
-        abilities: ['Rain-dish', 'Torrent'],
-        type: ['water']
-        },
-        {name: 'Wartortle',
-        pokedexNumber: 8,
-        height: 1,
-        abilities: ['Rain-dish', 'Torrent'],
-        type: ['water']
-        },
-        {name: 'Blastoise',
-        pokedexNumber: 9,
-        height: 1.6,
-        abilities: ['Rain-dish', 'Torrent'],
-        type: ['water']
-        }
-    ]
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -79,34 +25,51 @@ var pokemonRepository = (function () {
     pokemonList.appendChild(listPoke);
 
 }
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            //Add details to item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e)
+        });
+    }
     function showDetails(pokemon) {
-        console.log(pokemon.name);
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon.name);
+        });
     }
     return {
-        getAll: getAll,
         add: add,
-        addListItem: addListItem
+        getAll: getAll,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     };
 })();
 
-//creates a new variable that pushes the contents of the pokemonRepository.pokemonList to a global variable
-var pokemonListGLB = pokemonRepository.getAll()
-//creates a new function that calls and accesses the pokemonList to get information
-// function pokemon(pokemonListGLB) {
-//     //writes the pokemon's pokedex number and name
-//     document.write("#" + pokemonListGLB.pokedexNumber + " " + pokemonListGLB.name + " ");
-//     //creates a if-else-if statement that changes based on a pokemon height, and finishes writing to DOM
-//     if (pokemonListGLB.height >= 1.5) {
-//         document.write("(height: " + pokemonListGLB.height + ") - Wow that's big!</br>")
-//     }
-//     else if (pokemonListGLB.height < 1.5)
-//         document.write("(height: " + pokemonListGLB.height + ")</br>")
-// }
-
-//executes a forEach function using the paramater to call upon the pokemon function
-
-
-
-pokemonListGLB.forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon)
-})
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
+});
